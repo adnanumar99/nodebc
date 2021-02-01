5fefea245f437ca0bdad8631738407a2fd4fec38
Adnan Umar <adnanumar99@gmail.com>
2021-01-31 18:59:51 -0800

    added JS exercise

---
+++ js/index34.js
@@ -0,0 +1,7 @@
+var income = 1000
+revenue = 600
+taxes = 500
+
+console.log(800 >= income)
+console.log(400 >= taxes)
+console.log(600 == revenue)
---
+++ js/index37.js
@@ -0,0 +1,6 @@
+var average = a = 6
+studentAverage = b = 4
+pass = average - studentAverage
+
+console.log(`${studentAverage >= average}
+This student is missing ${pass} marks to pass`)
