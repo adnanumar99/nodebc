1fd1b28761f8ba9666d3bebb9af17536ded6ec4f
Adnan Umar <adnanumar99@gmail.com>
2021-01-31 20:01:57 -0800

    added JS exercises

---
+++ js/index39.js
@@ -0,0 +1,8 @@
+var firstName = 'Adnan'
+lastName = 'Umar'
+ 
+console.log(`My first name is ${firstName} and it is ${firstName.length} character long
+My last name is ${lastName} and it is ${lastName.length} character long
+The character difference between my first name and last name is ${firstName.length - lastName.length}
+My first name is longer than my last name is ${firstName.length > lastName.length}
+`)
